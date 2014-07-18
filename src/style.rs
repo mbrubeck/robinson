0ca29760580dd4ad4414f5f42abff828a762c93a
Matt Brubeck <mbrubeck@limpet.net>
2014-07-18 15:48:56 -0700

    Add some comments to the style module

--- src/style.rs
+++ src/style.rs
@@ -1,8 +1,16 @@
-use dom::{ElementData};
-use css::{Stylesheet, Rule};
-use css::{Selector, Simple, SimpleSelector};
+//! Code for applying CSS styles to the DOM.
+//!
+//! This is not very interesting at the moment.  It will get much more
+//! complicated if I add support for compound selectors.
 
+use dom::{ElementData};
+use css::{Stylesheet, Rule, Selector, Simple, SimpleSelector};
+
+/// Find all CSS rules that match the given element.
 pub fn matching_rules<'a>(elem: &ElementData, stylesheet: &'a Stylesheet) -> Vec<&'a Rule> {
+    // For now, we just do a linear scan of all the rules.  For large
+    // documents, it would be more efficient to store the rules in hash tables
+    // based on tag name, id, class, etc.
     stylesheet.rules.iter()
         .filter(|rule| rule.selectors.iter().any(|s| matches_selector(elem, s)))
         .collect()
