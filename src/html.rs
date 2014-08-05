d3138bd8110e70cfcc740b97fd017fdafc4f3ead
Matt Brubeck <mbrubeck@limpet.net>
2014-08-05 14:39:49 -0700

    Simplify HTML parser

--- src/html.rs
+++ src/html.rs
@@ -30,38 +30,49 @@ struct Parser {
 impl Parser {
     /// Parse a document and return the root element.
     fn parse_root(&mut self) -> dom::Node {
-        // TODO: Don't create a root <html> element if the input already contains one.
-        let mut root = dom::elem("html".to_string(), HashMap::new());
-        self.parse_nodes(&mut root);
-        root
+        let mut nodes = vec!();
+        self.parse_nodes(&mut nodes);
+
+        // If the document contains a root `<html>` element, just return it.
+        let has_root = nodes.len() == 1 && match nodes[0].node_type {
+            dom::Element(ref elem) if elem.tag_name.as_slice() == "html" => true,
+            _ => false
+        };
+        if has_root {
+            nodes.swap_remove(0).unwrap()
+        } else {
+            // If the root `<html>` element is missing, create one.
+            let mut root = dom::elem("html".to_string(), HashMap::new());
+            root.children.push_all_move(nodes);
+            root
+        }
     }
 
     /// Parse a sequence of sibling nodes.
-    fn parse_nodes(&mut self, parent: &mut dom::Node) {
+    fn parse_nodes(&mut self, nodes: &mut Vec<dom::Node>) {
         loop {
-            match self.parse_node() {
-                Some(node) => parent.children.push(node),
-                None => break
+            self.consume_whitespace();
+            if self.eof() || self.starts_with("</") {
+                break;
             }
+            nodes.push(self.parse_node());
         }
     }
 
     /// Parse a single node. Returns None if there is no node at the current position.
-    fn parse_node(&mut self) -> Option<dom::Node> {
-        self.consume_whitespace();
-        match (self.curr_char(), self.next_char()) {
-            (Some('<'), Some('/')) => None, // Unexpected end tag. Stop parsing nodes.
-            (Some('<'), Some(_))   => self.parse_element(),
-            (_, _)                 => self.parse_text()
+    fn parse_node(&mut self) -> dom::Node {
+        match self.curr_char() {
+            '<' => self.parse_element(),
+            _   => self.parse_text()
         }
     }
 
     /// Parse a single element, including its open tag, contents, and closing tag.
-    fn parse_element(&mut self) -> Option<dom::Node> {
+    fn parse_element(&mut self) -> dom::Node {
         let mut elem = self.parse_open_tag();
-        self.parse_nodes(&mut elem);
+        self.parse_nodes(&mut elem.children);
         self.consume_close_tag();
-        Some(elem)
+        elem
     }
 
     // Helper functions for parse_element:
@@ -69,28 +80,40 @@ impl Parser {
     fn parse_open_tag(&mut self) -> dom::Node {
         assert!(self.consume_char() == '<');
         let name = self.parse_tag_name();
+        let attrs = self.parse_attributes();
+        assert!(self.consume_char() == '>');
+
+        dom::elem(name, attrs)
+    }
+
+    fn parse_tag_name(&mut self) -> String {
+        self.consume_while(|c| match c {
+            'a'..'z' | 'A'..'Z' | '0'..'9' => true,
+            _ => false
+        })
+    }
+
+    fn parse_attributes(&mut self) -> dom::AttrMap {
         let mut attributes = HashMap::new();
         loop {
             self.consume_whitespace();
-            match self.curr_char().unwrap() {
+            match self.curr_char() {
                 '>' => {
-                    self.consume_char();
                     break;
                 }
-                _   => {
+                _ => {
                     let (name, value) = self.parse_attr();
                     attributes.insert(name, value);
                 }
             }
         }
-        dom::elem(name, attributes)
+        attributes
     }
 
+    // name="value"
     fn parse_attr(&mut self) -> (String, String) {
         let name = self.parse_tag_name();
-        self.consume_whitespace();
         assert!(self.consume_char() == '=');
-        self.consume_whitespace();
         let value = self.parse_attr_value();
         (name, value)
     }
@@ -98,30 +121,9 @@ impl Parser {
     fn parse_attr_value(&mut self) -> String {
         let open_quote = self.consume_char();
         assert!(open_quote == '"' || open_quote == '\'');
-        let mut value = String::new();
-        loop {
-            let c = self.consume_char();
-            if c == open_quote {
-                break;
-            }
-            value.push_char(c);
-        }
+        let mut value = self.consume_while(|c| c != open_quote);
+        assert!(!self.eof() && self.consume_char() == open_quote);
         value
-    }
-
-    fn parse_tag_name(&mut self) -> String {
-        let mut name = String::new();
-        loop {
-            let c = self.curr_char();
-            if c.is_none() {
-                break;
-            }
-            match c.unwrap() {
-                'a'..'z' | 'A'..'Z' | '0'..'9' => name.push_char(self.consume_char()),
-                _ => break,
-            }
-        }
-        name
     }
 
     fn consume_close_tag(&mut self) {
@@ -136,29 +138,22 @@ impl Parser {
     }
 
     /// Parse a text node.
-    fn parse_text(&mut self) -> Option<dom::Node> {
-        if self.eof() {
-            return None;
-        }
-        let mut data = String::new();
-        loop {
-            match self.curr_char() {
-                None      => break,
-                Some('<') => break,
-                _         => data.push_char(self.consume_char())
-            }
-        }
-        Some(dom::text(data))
+    fn parse_text(&mut self) -> dom::Node {
+        dom::text(self.consume_while(|c| c != '<'))
     }
 
     /// Consume and discard zero or more whitespace characters.
     fn consume_whitespace(&mut self) {
-        loop {
-            match self.curr_char() {
-                Some(c) if c.is_whitespace() => self.consume_char(),
-                _ => break,
-            };
+        self.consume_while(|c| c.is_whitespace());
+    }
+
+    /// Consume characters until `test` returns false.
+    fn consume_while(&mut self, test: |char| -> bool) -> String {
+        let mut result = String::new();
+        while !self.eof() && test(self.curr_char()) {
+            result.push_char(self.consume_char());
         }
+        result
     }
 
     /// Return the current character, and advance self.pos to the next character.
@@ -168,18 +163,15 @@ impl Parser {
         range.ch
     }
 
-    /// Read a character without consuming it.
-    fn char_at(&self, i: uint) -> Option<char> {
-        if i < self.input.len() {
-            Some(self.input.as_slice().char_at(i))
-        } else {
-            None
-        }
+    /// Read the current character without consuming it.
+    fn curr_char(&self) -> char {
+        self.input.as_slice().char_at(self.pos)
     }
 
-    // Convenience functions for reading ahead 0 or 1 characters.
-    fn curr_char(&self) -> Option<char> { self.char_at(self.pos)     }
-    fn next_char(&self) -> Option<char> { self.char_at(self.pos + 1) }
+    /// Does the current input start with the given string?
+    fn starts_with(&self, s: &str) -> bool {
+        self.input.as_slice().slice_from(self.pos).starts_with(s)
+    }
 
     /// Return true if all input is consumed.
     fn eof(&self) -> bool {
